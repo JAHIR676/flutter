0ebfcecf876140bc4b578fef85b96eb10f612c98
Jahir Abbas Sk <jahirabbas676@gmail.com>
2021-08-28 15:55:23 +0530

    validation implemented

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,147 +1,156 @@
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class SignUpPage extends StatefulWidget {
-  const SignUpPage({ Key? key }) : super(key: key);
+  const SignUpPage({Key? key}) : super(key: key);
   @override
   _SignUpPageState createState() => _SignUpPageState();
 }
 
 class _SignUpPageState extends State<SignUpPage> {
-   GlobalKey<FormState> formkey = new GlobalKey<FormState>();
-   final TextEditingController emailController  = new TextEditingController();
-   final TextEditingController passwordController = new TextEditingController();
-   final TextEditingController firstNameController = new TextEditingController();
-   final TextEditingController lastNameController = new TextEditingController();
-  
-   
+  GlobalKey<FormState> formkey = new GlobalKey<FormState>();
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+  final nameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(10.0),
           child: Column(
             children: [
-              SizedBox(height: 50,
+              SizedBox(
+                height: 50,
               ),
               Form(
-              key:  formkey,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                key: formkey,
                 child: Column(
                   children: [
-                    Text("Create An Account",
-              style: TextStyle(fontSize: 30,
-              fontWeight: FontWeight.bold
-             ),
-              ),
-              SizedBox(height: 70,
-              ),
-              TextFormField(
-              validator: (value){
-                if (value!.isEmpty){
-                  return "Enter Your First Name";
-                }
-                
-                else if(value.length<2){
-                  return "Least Two Character";
-              
-                }
-                else{
-                  return null;
-                }
-
-
-              },
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0)
-                  ),
-                  hintText: "Enter First Name",
-                  labelText: "First Name"
-                  
-                  
-                ),
-              ),
-              SizedBox(height: 15),
-              TextFormField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0)
-                  ),
-                  hintText: "Enter Last Name",
-                  labelText: "Last Name"
-                  
-                ),
-              ),
-              SizedBox(height: 15),
-
-              TextFormField(
-                
-                controller: emailController,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.mail),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0)
-                  ),
-                  hintText: "Enter Your Email",
-                  labelText: "Email"
-                  
-                ),
-              ),
-              SizedBox(height: 15),
-            
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.lock),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0)
-                  ),
-                  hintText: "Password",
-                  labelText: "Password"
-                  
-                ),
-              ),
+                    Text(
+                      "Create An Account",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 70,
+                    ),
+                    TextFormField(
+                      controller: nameController,
+                      keyboardType: TextInputType.text,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter Your Name";
+                        } else if (value.length < 2) {
+                          return "Atleast Two Character";
+                        } else {
+                          return null;
+                        }
+                      },
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0)),
+                          hintText: "Enter First Name",
+                          labelText: "First Name"),
+                    ),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter Your Name";
+                        } else if (value.length < 2) {
+                          return "Atleast Two Character";
+                        } else {
+                          return null;
+                        }
+                      },
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0)),
+                          hintText: "Enter Last Name",
+                          labelText: "Last Name"),
+                    ),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      validator: EmailValidator(errorText: "Enter Valid Email"),
+                      keyboardType: TextInputType.emailAddress,
+                      controller: emailController,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.mail),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0)),
+                          hintText: "Enter Your Email",
+                          labelText: "Email"),
+                    ),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      validator: MinLengthValidator(8,
+                          errorText: "Minimum 8 Character"),
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0)),
+                          hintText: "Password",
+                          labelText: "Password"),
+                    ),
                   ],
                 ),
-
               ),
-              
-              Padding(padding: EdgeInsets.all(20.0),
-              child: GestureDetector( onTap: (){
-                setState(() {
-                  formkey.currentState!.validate();
-                });
-              },
-                child: Container(
-                  height: 50,
-                  width: 200,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15.0),
-                    color: Colors.purple
-              
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Material(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        formkey.currentState!.validate();
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 200,
+                      child: Center(
+                        child: Text(
+                          "Signup",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                  child: Center(child: Text("Signup",
-                  style: TextStyle(color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  ),
-                  ),),
-                  
                 ),
               ),
-              )
+              Row(
+                children: [
+                  Padding(
+                      padding: EdgeInsets.all(60.0),
+                      child: Container(
+                        width: 40,
+                        height: 30,
+                        color: Colors.deepPurple,
+                      )),
+                  Padding(
+                    padding: EdgeInsets.all(50.0),
+                    child: Container(
+                        width: 40, height: 30, color: Colors.deepPurple),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
-
       ),
-      
     );
   }
 }
